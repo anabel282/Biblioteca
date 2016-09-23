9bafaaf5924920dd7d29bc25e82a215e30c2e24f
Curso <Curso@Formacion-TOSH6.ipartekaula.com>
2016-09-23 10:58:29 +0200

    rest sin vista

--- gestionBiblioteca/src/main/java/com/ipartek/formacion/rest/LibroRestController.java
+++ gestionBiblioteca/src/main/java/com/ipartek/formacion/rest/LibroRestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,46 +16,71 @@ import com.ipartek.formacion.service.interfaces.LibroService;
 
 @RequestMapping("/libroRest")
 public class LibroRestController {
-	
-	@Autowired
-	private LibroService libroService;
-	
-	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable("id") int id){
-		ResponseEntity<Void> respuesta = null;
-		if(this.libroService.getById(id) != null){
-			respuesta = new ResponseEntity<Void>(HttpStatus.OK);
-			this.libroService.delete(id);
-		}else{
-			respuesta = new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
-		}
-		return respuesta;
-	}
-	
-	@RequestMapping(value="/{id}", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Libro> getById(@PathVariable("id") int id){
-		ResponseEntity<Libro> respuesta = null;
-		Libro libro = this.libroService.getById(id);
-		if(libro != null || libro.getCodigo() > 0){
-			respuesta = new ResponseEntity<Libro>(libro, HttpStatus.OK);
-			this.libroService.delete(id);
-		}else{
-			respuesta = new ResponseEntity<Libro>(HttpStatus.NOT_FOUND);
-		}
-		return respuesta;
-	}
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<Libro>> getAll(){
-		ResponseEntity<List<Libro>> respuesta;
-		List<Libro> libros = libroService.getAll();
-		if(libros!=null && libros.size()>0){
-			respuesta = new ResponseEntity<List<Libro>>(libros, HttpStatus.OK);
-		}else{
-			respuesta = new ResponseEntity<List<Libro>>(HttpStatus.NOT_FOUND);
-		}
-		return respuesta;
-	}
-	
-	
+
+  @Autowired
+  private LibroService libroService;
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public ResponseEntity<Void> delete(@PathVariable("id") int id) {
+    ResponseEntity<Void> respuesta = null;
+    if (this.libroService.getById(id) != null) {
+      respuesta = new ResponseEntity<Void>(HttpStatus.OK);
+      this.libroService.delete(id);
+    } else {
+      respuesta = new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+    }
+    return respuesta;
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Libro> getById(@PathVariable("id") int id) {
+    ResponseEntity<Libro> respuesta = null;
+    Libro libro = this.libroService.getById(id);
+    if (libro != null || libro.getCodigo() > 0) {
+      respuesta = new ResponseEntity<Libro>(libro, HttpStatus.OK);
+      this.libroService.delete(id);
+    } else {
+      respuesta = new ResponseEntity<Libro>(HttpStatus.NOT_FOUND);
+    }
+    return respuesta;
+  }
+
+  @RequestMapping(method = RequestMethod.GET)
+  public ResponseEntity<List<Libro>> getAll() {
+    ResponseEntity<List<Libro>> respuesta;
+    List<Libro> libros = libroService.getAll();
+    if (libros != null && libros.size() > 0) {
+      respuesta = new ResponseEntity<List<Libro>>(libros, HttpStatus.OK);
+    } else {
+      respuesta = new ResponseEntity<List<Libro>>(HttpStatus.NOT_FOUND);
+    }
+    return respuesta;
+  }
+
+  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity<Libro> create(@RequestBody Libro libro) {
+
+    ResponseEntity<Libro> respuesta = null;
+    if (libroService.getById(libro.getCodigo()) != null) {
+      respuesta = new ResponseEntity<Libro>(HttpStatus.OK);
+      libroService.create(libro);
+    } else {
+      respuesta = new ResponseEntity<Libro>(HttpStatus.NOT_FOUND);
+    }
+    return respuesta;
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+  public ResponseEntity<Libro> update(@PathVariable("id") int id, @RequestBody Libro libro) {
+
+    ResponseEntity<Libro> respuesta = null;
+    if (libroService.getById(id) != null) {
+      respuesta = new ResponseEntity<Libro>(HttpStatus.OK);
+      libroService.update(libro);
+    } else {
+      respuesta = new ResponseEntity<Libro>(HttpStatus.NOT_FOUND);
+    }
+    return respuesta;
+  }
+
 }
